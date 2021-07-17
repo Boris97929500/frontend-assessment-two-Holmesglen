72b7ee6293af719d8c30f74bd41ca7798933aa2f
Boris <boris@email.com>
2021-07-17 20:26:20 +1000

    navbar second commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { style } from '@material-ui/system';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -18,6 +19,13 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+    navlink: {
+        color: "white",
+        textDecoration: "none",
+        padding: "0 1.1rem",
+        fontWeight: "500",
+        fontFamily: "Roboto, Helvetica, Arial, sans-serif"
+    }
 }));
 
 export default function Navbar() {
@@ -33,10 +41,10 @@ export default function Navbar() {
                     <Typography variant="h6" className={classes.title}>
                         Moochi
                     </Typography>
-                    <Link to="/form">Form</Link>
-                    <Link to="/list">List</Link>
-                    <Link to="/table">Table</Link>
-                    <Link to="/shop">Company Details</Link>
+                    <Link className={classes.navlink} to="/form">Form</Link>
+                    <Link className={classes.navlink} to="/list">List</Link>
+                    <Link className={classes.navlink} to="/table">Table</Link>
+                    <Link className={classes.navlink} to="/shop">Company Details</Link>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
