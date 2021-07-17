15ce7d04707078a7be3b34cae86ee1f0ab39b373
Boris <boris@email.com>
2021-07-17 19:37:18 +1000

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Shop from "./Shop.js";
+import Form from "./Form";
+import List from "./List";
+import Table from "./Table";
+import { Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Route path="/shop" exact component={Shop} />
+                <Route path="/form" exact component={Form} />
+                <Route path="/list" exact component={List} />
+                <Route path="/table" exact component={Table} />
+            </div>
+        )
+    }
 }
 
 export default App;
