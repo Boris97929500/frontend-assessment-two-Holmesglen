006588de2f5785abfc4ef72b0e833ce282660f45
Boris <boris@email.com>
2021-07-18 11:49:50 +1000

    footer polished

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+
+    footer: {
+       padding: '1rem 1rem',
+        backgroundColor: '#169ae9',
+        borderRadius: '3px',
+    },
+    footerIcon:{
+        padding: '0 1rem',
+    }
+}));
 
 export default function Footer() {
+
+    const classes = useStyles();
     return (
-        <div>
+        <div className={classes.footer}>
             <footer>
-                <a href="https://www.facebook.com">Facebook</a>
-                <p>       </p>
-                <a href="https://twitter.com/?lang=en">Twitter</a>
+                <a className={classes.footerIcon} href="https://www.facebook.com"><img src="../image/facebook_icon.png" /></a>
+                <a className={classes.footerIcon}  href="https://twitter.com/?lang=en"><img src="../image/twitter_icon.png" /></a>
+                
             </footer>
+
         </div>
     )
 }
